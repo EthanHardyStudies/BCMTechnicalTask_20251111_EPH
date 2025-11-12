1344f08ebfaf29edb8d8fd410c776ded46bc62ad
EthanHardyStudies <ethan.hardy@iu-study.org>
2025-11-12 19:18:48 +0200

    Update customer model with validation method. Change input format for console app.

--- BCMTechnicalTask_20251111_EPH/Models/Customer.cs
+++ BCMTechnicalTask_20251111_EPH/Models/Customer.cs
@@ -14,6 +14,36 @@ namespace BCMTechnicalTask.Models
         public int CreditUtilization { get; set; }
         public int AgeOfCreditHistory { get; set; }
         public double CreditScore { get; set; }
+
+        public string Validate()
+        {
+            string _response = "";
+            if (CustomerId <= 0)
+            {
+                _response += "CustomerID error: Invalid value provided, customer IDs cannot be less than 1.";
+            }
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                if (_response.Length > 0) { _response += "\n"; }
+                _response += "Name error: Name cannot be null or blank.";
+            }
+            if (PaymentHistory < 0 || PaymentHistory > 100)
+            {
+                if (_response.Length > 0) { _response += "\n"; }
+                _response += "PaymentHistory error: Payment History must be a whole number between 0 and 100.";
+            }
+            if (CreditUtilization < 0 || CreditUtilization > 100)
+            {
+                if (_response.Length > 0) { _response += "\n"; }
+                _response += "CreditUtilization error: Credit Utilization must be a whole number between 0 and 100.";
+            }
+            if (AgeOfCreditHistory < 0)
+            {
+                if (_response.Length > 0) { _response += "\n"; }
+                _response += "Age Of Credit History error: Age of Credit history must be greater than or equal to 0.";
+            }
+            return _response;
+        }
     }
 
     public class CustomerOutput
--- BCMTechnicalTask_20251111_EPH/Program.cs
+++ BCMTechnicalTask_20251111_EPH/Program.cs
@@ -5,6 +5,7 @@ using System.Data;
 using Newtonsoft;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace BCMTechnicalTask
 {
@@ -17,94 +18,41 @@ namespace BCMTechnicalTask
             Console.WriteLine("|=============== BCM Customer Credit Score Calculator ===============| \n");
             while(_addingCustomers)
             {
-                CustomerInput _customer = new CustomerInput();
-
-                //Ask user to enter the customer ID
-                int _idTemp; //Create local variable to try parse the integer input for the id
-                bool _validID = false; //declare boolean to allow for looping if the input is not an integer.
-                while (!_validID)
+                _customerList = null;// In case the data must be entered again due to parsing or validation errors, nullify the list.
+                Console.Write("\nEnter customer data:\n");
+                bool _validData = true;
+                
+                StringBuilder _inputData = new StringBuilder(); //String builder to allow for appending multiple lines together.
+                string line;
+                while (!string.IsNullOrWhiteSpace(line = Console.ReadLine())) //loop through the lines of the console until an empty line is entered. This allows multi line entries.
                 {
-                    Console.Write("\nEnter customer ID:\n");
-                    bool _idParse = int.TryParse(Console.ReadLine(), out _idTemp); //Try parse the input value
-                    if (_idParse && _idTemp >= 1) //Check if value was parsed correctly and is positive
-                    {
-                        _customer.CustomerId = _idTemp; //update customer attribute using temp variable.
-                        _validID = true; //update the loop condition to exit the loop.
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid customer ID of type integer."); //if the value was not parsed successfully display and error for the user.
-                    }
+                    _inputData.Append(line);
                 }
 
-
-                //Ask user to enter the customers name
-                Console.Write("\nEnter the customers name:\n");
-                _customer.Name = Console.ReadLine();
-
-                //Ask user to enter the payment history score
-                int _payHistTemp; //Create local variable to try parse the integer input
-                bool _validPayHist = false; //declare boolean to allow for looping if the input is not an integer.
-                while (!_validPayHist)
+                //Try parse the entered data. provide an error if the parsing fails.
+                try
                 {
-                    Console.Write("\nEnter customers payment history score (0 - 100):\n");
-                    bool _payHistParse = int.TryParse(Console.ReadLine(), out _payHistTemp); //Try parse the input value
-                    if (_payHistParse && (_payHistTemp >= 0 && _payHistTemp <= 100))//Check if value was parsed correctly and is between 0 and 100
+                    _customerList = JsonConvert.DeserializeObject<List<CustomerInput>>(_inputData.ToString());
+                    
+                    foreach (CustomerInput _customer in _customerList)
                     {
-                        _customer.PaymentHistory = _payHistTemp;//update customer attribute using temp variable.
-                        _validPayHist = true;//update the loop condition to exit the loop.
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid payment history score of type integer.");//if the value was not parsed successfully display and error for the user.
+                        string _validation = _customer.Validate();
+                        if (_validation.Length > 0)
+                        {
+                            _validData = false;
+                            Console.WriteLine("Invalid data entered. CustomerID: " + _customer.CustomerId.ToString() + " Errors: \n" + _validation);
+                        }
                     }
                 }
-
-                //Ask user to enter the credit utilization score
-                int _creditUtTemp; //Create local variable to try parse the integer input
-                bool _validcreditUt = false; //declare boolean to allow for looping if the input is not an integer.
-                while (!_validcreditUt)
+                catch (Exception ex)
                 {
-                    Console.Write("\nEnter the customers credit utilization score (0 - 100):\n");
-                    bool _creditUtParse = int.TryParse(Console.ReadLine(), out _creditUtTemp); //Try parse the input value
-                    if (_creditUtParse && (_creditUtTemp >= 0 && _creditUtTemp <= 100))//Check if value was parsed correctly and is between 0 and 100
-                    {
-                        _customer.CreditUtilization = _creditUtTemp;//update customer attribute using temp variable.
-                        _validcreditUt = true;//update the loop condition to exit the loop.
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid credit utilization score of type integer.");//if the value was not parsed successfully display and error for the user.
-                    }
-                }
+                    Console.WriteLine("Unable to parse input. Error: " + ex.Message);
+                    _validData = false;
+                }                
 
-                //Ask user to enter the age of the customers credit history
-                int _creditAgeTemp;//Create local variable to try parse the integer input
-                bool _validCreditAge = false; //declare boolean to allow for looping if the input is not an integer.
-                while (!_validCreditAge)
+                if (_validData)
                 {
-                    Console.Write("\nEnter the age of the customers credit history (in years):\n");
-                    bool _creditAgeParse = int.TryParse(Console.ReadLine(), out _creditAgeTemp); //Try parse the input value
-                    if (_creditAgeParse && _creditAgeTemp >= 0) //Check if value was parsed correctly and is positive
-                    {
-                        _customer.AgeOfCreditHistory = _creditAgeTemp;//update customer attribute using temp variable.
-                        _validCreditAge = true;//update the loop condition to exit the loop.
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter a valid credit history age of type integer.");//if the value was not parsed successfully display and error for the user.
-                    }
-                }
-
-                //Add customer into object list that will be processed.
-                _customerList.Add(_customer); 
-
-                //Ask user to decide if they want to continue adding customers
-                Console.Write("\nWould you like to input another customers details for your report? (Y/N)\n");
-                string _continue = Console.ReadLine();
-                if (_continue.ToLower() == "n") 
-                {
-                    _addingCustomers = false;//Break the while loop if the customer does not want to add another customer.
+                    _addingCustomers = false;
                 }
             }
 
@@ -113,16 +61,9 @@ namespace BCMTechnicalTask
             //Call the method to calculate the credit scores for customers.
             List<CustomerOutput> _results = CreditScoreCalculations.CalculateCreditScore(_customerList);
 
-            //loop through and print results to console
-            for (int i = 0; i < _results.Count; i++)
-            {
-                Console.WriteLine("|=== Customer Number " + (i + 1).ToString() + " ===|\n\n"
-                    + "Customer Name: " + _results[i].Name + "\n"
-                    + "Credit Score: " + _results[i].CreditScore.ToString() + "\n"
-                    + "Risk profile: " + _results[i].RiskProfile + "\n");
-            }
-
             JArray _printResults = JsonConvert.DeserializeObject<JArray>(JsonConvert.SerializeObject(_results.ToArray())); //convert list to json array for printing to file
+            Console.WriteLine(JsonConvert.SerializeObject(_printResults, Formatting.Indented)); //Write results to the console in json array format
+            
             File.WriteAllText("CreditResults.json", _printResults.ToString(Formatting.Indented)); //Print result array to json file.
             Console.WriteLine("\n|=============== Thank you for using this service. ===============| \n");
         }
